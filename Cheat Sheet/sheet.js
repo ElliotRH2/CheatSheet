9df74f91a2f0455bba55e8588a7a6932722a2b09
Glowing-Red <79102132+Glowing-Red@users.noreply.github.com>
2024-02-12 14:07:42 +0100

    sidebar sheet

--- Cheat Sheet/sheet.js
+++ Cheat Sheet/sheet.js
@@ -5,4 +5,27 @@ function copyCode() {
    }).catch(err => {
       console.error('Failed to copy:', err);
    });
-} 
+}
+
+var anchor = document.getElementById("Dropdown");
+var dropdowns = document.querySelectorAll(".dropdown");
+var test = false;
+
+anchor.addEventListener("click", function(event) {
+   console.log(event, test);
+   if (test == false) {
+      test = true;
+
+      anchor.querySelector("img").src = "arrow.svg";
+      dropdowns.forEach(function(dropdown) {
+         dropdown.style.display = "none";
+      });
+   } else {
+      test = false;
+
+      anchor.querySelector("img").src = "arrow_down.svg";
+      dropdowns.forEach(function(dropdown) {
+         dropdown.style.display = "block";
+      });
+   };
+});
