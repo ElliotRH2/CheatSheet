9bee4e8d1b5877bd1a8b096f18037bb4a5eec078
ElliotRH <elliot.a.rhoden@gmail.com>
2024-02-15 13:18:48 +0100

    push

--- Cheat Sheet/cssSheet/border.js
+++ Cheat Sheet/cssSheet/border.js
@@ -4,26 +4,72 @@ const paddingInput = document.getElementById('paddingInput')
 const borderBox = document.getElementById('borderBox');
 const borderBox2 = document.getElementById('borderBox2')
 
+const maxBorder = 50
+const maxPadding = 100
+
 radiusInput.addEventListener('input', () => {
     const radiusValue = radiusInput.value + 'px';
     borderBox.style.borderRadius = radiusValue;
   });
 
-  borderSizeInput.addEventListener('input', () => {
+borderSizeInput.addEventListener('input', () => {
     const borderSizeValue = borderSizeInput.value + 'px';
     borderBox.style.borderWidth = borderSizeValue;
   });
 
-  borderStyleInput.addEventListener('change', () => {
+borderStyleInput.addEventListener('change', () => {
     const borderStyleValue = borderStyleInput.value;
     borderBox.style.borderStyle = borderStyleValue;
   });
 
-  paddingInput.addEventListener('input', () => {
+paddingInput.addEventListener('input', () => {
     const paddingValue = paddingInput.value + 'px';
     borderBox2.style.padding = paddingValue;
   });
 
-  
+paddingInput.addEventListener('input', () => {
+    let value = parseInt(paddingInput.value);
+    if (value > maxPadding) {
+      paddingInput.value = maxPadding;
+    }
+  });
 
-  paddingBox.style.backgroundColor = '#ff7b2e';
+paddingInput.addEventListener('keypress', (event) => {
+    let value = parseInt(paddingInput.value + event.key);
+    if (value > maxPadding) {
+      event.preventDefault();
+    }
+  });
+  
+borderSizeInput.addEventListener('input', () => {
+    let value = parseInt(borderSizeInput.value);
+    if (value > maxBorder) {
+      borderSizeInput.value = maxBorder;
+    }
+  });
+
+borderSizeInput.addEventListener('keypress', (event) => {
+    let value = parseInt(borderSizeInput.value + event.key);
+    if (value > maxBorder) {
+      event.preventDefault();
+    }
+  });
+
+const fontSelect = document.getElementById('fontSelect');
+const textElement = document.getElementById('textElement');
+const boldCheckbox = document.getElementById('boldCheckbox');
+
+fontSelect.addEventListener('change', () => {
+  const selectedFont = fontSelect.value;
+  textElement.style.fontFamily = selectedFont;
+});
+
+fontSelect.addEventListener('change', applyFontStyles);
+boldCheckbox.addEventListener('change', applyFontStyles);
+
+function applyFontStyles() {
+  let fontWeight = boldCheckbox.checked ? 'bold' : 'normal';
+  textElement.style.fontWeight = fontWeight;
+  const selectedFont = fontSelect.value;
+  textElement.style.fontFamily = selectedFont;
+}
